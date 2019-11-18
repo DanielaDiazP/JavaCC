8c03dab20c8626e6047fd172d22bf31e5103e5d4
DanielaDiazP <dani092012@hotmail.com>
2019-11-17 21:52:26 -0500

    second commit

--- src/main/resources/org/daniela/drawer/model/alt.java
+++ src/main/resources/org/daniela/drawer/model/alt.java
@@ -2,4 +2,24 @@ package org.daniela.drawer.model;
 
 public class alt {
 
+  /**
+   *
+   * @author alvar
+   */
+  public class alt extends elementoSintactico {
+
+    private String identificador;
+
+    public alt(String identificador) {
+      this.identificador = identificador;
+    }
+
+    @Override
+    public String parse() {
+      throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                     // Tools | Templates.
+    }
+
+  }
+
 }
